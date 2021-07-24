cfeb4a59fb40d23561086daf5d76882befe9aba9
Simran Kaur <kaursim.2019@yahoo.com>
2021-07-23 22:13:58 -0700

    updates

---
+++ Develop/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+// const api = require('./public/assets/js/index.js');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+// app.use('/api', api);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
